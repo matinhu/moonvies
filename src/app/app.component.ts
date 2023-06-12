6a3aeba5b602d3ab693318b603ad85c295d48ada
@matinhu <matheulls@hotmail.com>
2023-06-12 17:50:15 -0300

    cartao da shem KKKKKKKKK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectorRef, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { HttpService } from 'src/app/services/http.service';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,7 @@ import { HttpService } from 'src/app/services/http.service';
   encapsulation: ViewEncapsulation.None
 
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'moonvies';
 
   public inProgress: Observable<boolean>;
@@ -19,15 +20,112 @@ export class AppComponent {
     private httpService: HttpService,
     private cdref: ChangeDetectorRef,
     private router: Router
-  ) { 
+  ) {
     this.inProgress = this.httpService.inProgress;
   }
+
+
+  list = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',];
+  subscribeTimer: any;
+  timeLeft = 10;
+
+  ngOnInit() {
+    this.observableTimer();
+  }
+
+
   ngAfterContentChecked() {
     this.cdref.detectChanges();
+    this.startPages();  
+
   }
 
   goTo(url: string) {
     this.router.navigate([url]);
 
   }
+
+
+
+  openEnvelope() {
+
+    const letter = document.querySelector('.letter');
+    if (letter) {
+
+      if (letter.classList.contains('letter--open')) {
+        letter.classList.remove('letter--open');
+        letter.classList.add('letter--close');
+        setTimeout(function () {
+          letter.classList.remove('letter--close');
+        }, 600);
+      } else {
+        letter.classList.remove('letter--close');
+        letter.classList.add('letter--open');
+      }
+
+
+
+    }
+
+
+
+  }
+
+  closeEnvelope() {
+    const letter = document.querySelector('.letter');
+    if (letter) {
+      letter.classList.remove('letter--open');
+      letter.classList.add('letter--close');
+      setTimeout(function () {
+        letter.classList.remove('letter--close');
+      }, 600);
+    }
+  }
+
+
+
+
+
+  observableTimer() {
+    const source = timer(1000, 1000);
+    const abc = source.subscribe(val => {
+      this.subscribeTimer = this.timeLeft - val;
+      console.log(this.subscribeTimer)
+    });
+  }
+
+
+
+  startPages() {
+    const pages: any = document.getElementsByClassName('page');
+    for (var i = 0; i < pages.length; i++) {
+      var page = pages[i];
+      if (i % 2 === 0) {
+        page['style'].zIndex = (pages.length - i);
+      }
+    }
+
+
+
+    for (var i = 0; i < pages.length; i++) {
+      //Or var page = pages[i];
+      pages[i].pageNum = i + 1;
+      pages[i].onclick = function () {
+        if (this.pageNum % 2 === 0) {
+          this.classList.remove('flipped');
+          this.previousElementSibling.classList.remove('flipped');
+        }
+        else {
+          this.classList.add('flipped');
+          this.nextElementSibling.classList.add('flipped');
+        }
+      }
+    }
+
+
+  }
+
+
+
+
 }
